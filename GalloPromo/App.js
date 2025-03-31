1ca397bae0e6c2842d8503ad4a92b5edb1c0dc3b
7diegojr <dfsntsjunior@gmail.com>
2025-03-31 18:13:52 -0300

    Iniciando o projeto

--- GalloPromo/App.js
+++ GalloPromo/App.js
@@ -1,12 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
+    <SafeAreaView style={styles.container}>
+      <View style={titleBox}>
+        <Text style={titleText}>GalloPromo</Text>
+      </View>
+
+      <View style={styles.content}>
+        <Text style={styles.subTitle}>Calculadora de Promoções</Text>
+        <View>
+          <Text style={styles.label}>Valor do produto:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setHeight}
+            value={height ?? ''}
+            placeholder='Ex. 100,00'
+            keyboardType='numeric'
+          />
+        </View>
+
+        <View style={{marginTop: 25}}>
+          <Text style={styles.label}>Porcentagem de Promoção:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setWeight}
+            value={height ?? ''}
+            placeholder='Ex. 20%'
+            keyboardType='numeric'
+          />
+        </View>
+
+        <TouchableOpacity style={style.button}
+        onPress={() => validatePreco()}>
+          
+        </TouchableOpacity>
+
+
+      </View>
+
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
   );
 }
 
