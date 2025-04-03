62730b16ee8892341995b2c8df903ac072455e59
7diegojr <dfsntsjunior@gmail.com>
2025-04-03 12:26:52 -0300

    Desenvolvimento

--- GalloPromo/App.js
+++ GalloPromo/App.js
@@ -1,49 +1,97 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, TextInput, Touchable, TouchableOpacity, Keyboard } from 'react-native';
+import Ionicons from "@expo/vector-icons/Ionicons"
+import { Image } from 'expo-image';
 
 export default function App() {
+  const [height, setHeight] = useState(null);
+  const [weight, setWeight] = useState(null);
+  const [imc, setDesc] = useState(null);
+  const [textButton, setTextButton] = useState("Calcular");
+  const [messageImc, setMessageImc] = useState("Preencha o peso e altura.");
+
+  function imcCalculator() {
+    //Preço / altura ** 2
+    setDesc((weight / height ** 2).toFixed(2))
+  }
+
+  function validateImc() {
+    if (weight != null && height != null)
+    {
+      Keyboard.dismiss();
+      imcCalculator();
+      setHeight(null);
+      setWeight(null);
+      setTextButton("Calcular novamente.");
+      setMessageImc("O valor:");
+      return;  
+    }
+    setDesc(null);
+    setTextButton("Calcular");
+    setMessageImc("Preencha o peso e altura.")
+  }
+
   return (
+
+
     <SafeAreaView style={styles.container}>
-      <View style={titleBox}>
-        <Text style={titleText}>GalloPromo</Text>
-      </View>
+      <SafeAreaView style={styles.titleBox}>
+        <Text style={styles.titleText}>Gallo Promo</Text>
+        <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require('./img/Gallogo.png')}
+        placeholder={{ blurhash }}
+        contentFit="cover"
+        transition={1000}
+      />
+    </View>
+      </SafeAreaView>
+
+      
 
       <View style={styles.content}>
-        <Text style={styles.subTitle}>Calculadora de Promoções</Text>
-        <View>
-          <Text style={styles.label}>Valor do produto:</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={setHeight}
-            value={height ?? ''}
-            placeholder='Ex. 100,00'
-            keyboardType='numeric'
-          />
-        </View>
+      <Text style={styles.subTitle}>Calculadora de Promoções</Text>
 
-        <View style={{marginTop: 25}}>
-          <Text style={styles.label}>Porcentagem de Promoção:</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={setWeight}
-            value={height ?? ''}
-            placeholder='Ex. 20%'
-            keyboardType='numeric'
-          />
-        </View>
+      <View>
+        <Text style={styles.label}>Preço do Produto:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setHeight}
+          value={height ?? ''}
+          placeholder='Digite o preço...'
+          keyboardType='numeric'
+        />
+      </View>
 
-        <TouchableOpacity style={style.button}
-        onPress={() => validatePreco()}>
-          
-        </TouchableOpacity>
+      <View style={{ marginTop:25 }}>
+        <Text style={styles.label}>Valor do Desconto:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setWeight}
+          value={weight ?? ''}
+          placeholder='Digite o desconto...'
+          keyboardType='numeric'
+        />
+      </View>
 
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => validateImc()}
+       >
+        <Ionicons name={"calculator-sharp"} size={24} color={"#edf2f4"} />
+        <Text style={styles.text}>{textButton}</Text>
+      </TouchableOpacity>
+
+      <View style={styles.imcContainer}>
+        <Text style={styles.imcText}>{messageImc}</Text>
+        <Text style={styles.imcResult}>{imc}</Text>
+      </View>
 
       </View>
 
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <StatusBar style="light" />
     </SafeAreaView>
   );
 }
@@ -51,21 +99,80 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#edf1f4',
+    backgroundColor: '#edf2f4',
   },
-
-  TitleBox: {
-    alignItems: "center",
+  titleBox: {
+    alignItems: 'center',
     justifyContent: 'flex-end',
-    height:100,
-    backgroundColor:'#edf1f4',
+    height: 100,
+    backgroundColor: '#221f62',
     borderBottomStartRadius: 25,
-    borderBottomEndRadius:25,
+    borderBottomEndRadius: 25,
   },
   titleText: {
-    color:'#edf1f4',
-    fontSize:28,
-    fontWeight:'bold',
-    marginBottom:20,
+    color: '#edf2f4',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  content: {
+    flex: 1,
+    padding: 40,
+    width: '100%',
+    backgroundColor: '#edf2f4'
+  },
+  subTitle: {
+    fontSize: 23,
+    textAlign: 'center',
+    color: '#221f62',
+    fontWeight: 'bold',
+    marginBottom: 40
+  },
+  label: {
+    color: '#221f62',
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  input: {
+    height: 48,
+    width: '100%',
+    fontSize: 18,
+    borderColor: '#221f62',
+    borderWidth: 2,
+    borderRadius: 18,
+    marginVertical: 7,
+  },
+  button: {
+    width: '100%',
+    paddingVertical: 15,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#cf2323',
+    borderRadius: 15,
+    marginTop: 40,
+    marginBottom: 10
+  },
+  text: {
+    color: '#edf2f4',
+    fontSize: 24,
+    fontWeight: '600',
+    marginLeft: 5
+  },
+  imcContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  imcResult: {
+    fontSize: 48,
+    color: '#cf2323',
+    fontWeight: 'bold'
+  },
+  imcText: {
+    fontSize: 18,
+    color: '#221f62',
+    fontWeight: 'bold'
   }
 });
